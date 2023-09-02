d9a73ba402363cb19787e1f0203178e8be4b2020
julivz2004 <52061972+julivz2004@users.noreply.github.com>
2023-09-01 20:35:38 -0500

    Add files via upload

---
+++ Numero/prueba.py
@@ -0,0 +1,17 @@
+Estudiante={'id':123
+           ,'Nombre':'Andrea'
+           ,'Asignatura':['Bd','web2']
+           ,'Estudiante1':{
+                 'id':124,
+                 'Nombre':'Andres',
+                 'asignatura':['Logica'],  
+             }}
+
+# Estudiante['cell']=3204509876
+
+# Estudiante['Nombre']='Andres'
+
+print(Estudiante)
+
+# for i,Estudiante in Estudiante.items():
+#     print(i,':',Estudiante.values())
